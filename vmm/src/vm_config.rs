510546bea2403631c74b5cd34035b6e252f55775
Stefan Kober <stefan.kober@cyberus-technology.de>
2025-09-11 13:25:21 +0200

    console: add tcp option
    In addition to configuration options like pty, file, tty, ... we allow
    setting the serial device to be accessed via some open TCP port on the
    host.
    
    Signed-off-by: Stefan Kober <stefan.kober@cyberus-technology.de>

--- vmm/src/vm_config.rs
+++ vmm/src/vm_config.rs
@@ -496,6 +496,7 @@ pub enum ConsoleOutputMode {
     Tty,
     File,
     Socket,
+    Tcp,
     Null,
 }
 
@@ -507,6 +508,7 @@ pub struct ConsoleConfig {
     #[serde(default)]
     pub iommu: bool,
     pub socket: Option<PathBuf>,
+    pub url: Option<String>,
 }
 
 pub fn default_consoleconfig_file() -> Option<PathBuf> {
@@ -856,6 +858,7 @@ pub fn default_serial() -> ConsoleConfig {
         mode: ConsoleOutputMode::Null,
         iommu: false,
         socket: None,
+        url: None,
     }
 }
 
@@ -865,6 +868,7 @@ pub fn default_console() -> ConsoleConfig {
         mode: ConsoleOutputMode::Tty,
         iommu: false,
         socket: None,
+        url: None,
     }
 }
 
