e96a6b66a739cdaceb3497cc454bbe7802b2f21c
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-09-11 09:30:55 +0200

    vmm: optimize lock usage
    No need to grab the lock multiple times in this short period
    of time. The lock is anyway held for the duration of the long
    operation (KVM_RUN).
    
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>
    On-behalf-of: SAP philipp.schuster@sap.com

--- vmm/src/cpu.rs
+++ vmm/src/cpu.rs
@@ -1160,12 +1160,14 @@ impl CpuManager {
 
                                 #[cfg(feature = "kvm")]
                                 if matches!(hypervisor_type, HypervisorType::Kvm) {
-                                    vcpu.lock().unwrap().vcpu.set_immediate_exit(true);
-                                    if !matches!(vcpu.lock().unwrap().run(), Ok(VmExit::Ignore)) {
+                                    let lock = vcpu.lock();
+                                    let mut lock = lock.unwrap();
+                                    lock.vcpu.set_immediate_exit(true);
+                                    if !matches!(lock.run(), Ok(VmExit::Ignore)) {
                                         error!("Unexpected VM exit on \"immediate_exit\" run");
                                         break;
                                     }
-                                    vcpu.lock().unwrap().vcpu.set_immediate_exit(false);
+                                    lock.vcpu.set_immediate_exit(false);
                                 }
 
                                 vcpu_run_interrupted.store(true, Ordering::SeqCst);
