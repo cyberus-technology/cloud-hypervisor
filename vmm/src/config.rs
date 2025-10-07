ba0dbf99e2e2297b957beda46139f98f611b5adb
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2025-10-07 05:34:22 +0200

    misc: Make CPU profile part of various configs
    We integrate the CPU profile into the various configs that
    ultimately get set by the user.
    
    This quickly ends up involving multiple files, luckily Rust
    helps us find which ones via compilation errors.
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- vmm/src/config.rs
+++ vmm/src/config.rs
@@ -11,6 +11,7 @@ use std::result;
 use std::str::FromStr;
 use std::sync::LazyLock;
 
+use arch::CpuProfile;
 use block::ImageType;
 use clap::ArgMatches;
 use log::{debug, warn};
@@ -695,7 +696,8 @@ impl CpusConfig {
             .add("features")
             .add("nested")
             .add("core_scheduling")
-            .add("nested");
+            .add("nested")
+            .add("profile");
         parser.parse(cpus).map_err(Error::ParseCpus)?;
 
         let boot_vcpus: u32 = parser
@@ -727,6 +729,12 @@ impl CpusConfig {
                     })
                     .collect()
             });
+
+        let profile = parser
+            .convert::<CpuProfile>("profile")
+            .map_err(Error::ParseCpus)?
+            .unwrap_or_default();
+
         let features_list = parser
             .convert::<StringList>("features")
             .map_err(Error::ParseCpus)?
@@ -769,6 +777,7 @@ impl CpusConfig {
             features,
             nested,
             core_scheduling,
+            profile,
         })
     }
 }
