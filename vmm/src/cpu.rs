6a50ea359ca240de7fd5e30fb8038f35903e887b
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2025-10-07 05:34:22 +0200

    misc: Make CPU profile part of various configs
    We integrate the CPU profile into the various configs that
    ultimately get set by the user.
    
    This quickly ends up involving multiple files, luckily Rust
    helps us find which ones via compilation errors.
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- vmm/src/cpu.rs
+++ vmm/src/cpu.rs
@@ -1001,6 +1001,7 @@ impl CpuManager {
                     #[cfg(feature = "tdx")]
                     tdx,
                     amx: self.config.features.amx,
+                    profile: self.config.profile,
                 },
             )
             .map_err(Error::CommonCpuId)?
--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -1924,17 +1924,27 @@ impl Vmm {
                 )));
             }
 
-            let amx = vm_config.lock().unwrap().cpus.features.amx;
-            let phys_bits =
-                vm::physical_bits(hypervisor, vm_config.lock().unwrap().cpus.max_phys_bits);
+            let (amx, max_phys_bits, profile, kvm_hyperv) = {
+                let guard = vm_config.lock().unwrap();
+                (
+                    guard.cpus.features.amx,
+                    guard.cpus.max_phys_bits,
+                    guard.cpus.profile,
+                    guard.cpus.kvm_hyperv,
+                )
+            };
+
+            let phys_bits = vm::physical_bits(hypervisor, max_phys_bits);
+
             arch::generate_common_cpuid(
                 hypervisor,
                 &arch::CpuidConfig {
                     phys_bits,
-                    kvm_hyperv: vm_config.lock().unwrap().cpus.kvm_hyperv,
+                    kvm_hyperv,
                     #[cfg(feature = "tdx")]
                     tdx: false,
                     amx,
+                    profile,
                 },
             )
             .context("Error generating common cpuid")
@@ -2109,6 +2119,7 @@ impl Vmm {
                     #[cfg(feature = "tdx")]
                     tdx: false,
                     amx: vm_config.cpus.features.amx,
+                    profile: vm_config.cpus.profile,
                 },
             )
             .context("Error generating common cpuid")
@@ -3484,6 +3495,8 @@ const DEVICE_MANAGER_SNAPSHOT_ID: &str = "device-manager";
 mod unit_tests {
     use std::path::PathBuf;
 
+    use arch::CpuProfile;
+
     use super::*;
     #[cfg(target_arch = "x86_64")]
     use crate::vm_config::DebugConsoleConfig;
@@ -3521,6 +3534,7 @@ mod unit_tests {
                 features: CpuFeatures::default(),
                 nested: true,
                 core_scheduling: CoreScheduling::default(),
+                profile: CpuProfile::default(),
             },
             memory: MemoryConfig {
                 size: 536_870_912,
