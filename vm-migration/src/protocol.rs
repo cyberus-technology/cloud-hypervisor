3c26063dcf190ae7aaf90e33dff5156a45548a7b
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-10-21 16:38:28 +0200

    vm-migration: allow partitioning memory tables
    For sending memory over multiple connections, we need a way to split
    up the work. With these changes, we can take a memory table and chop
    it into same-sized chunks for transmit.
    
    On-behalf-of: SAP julian.stecklina@sap.com
    Signed-off-by: Julian Stecklina <julian.stecklina@cyberus-technology.de>

--- vm-migration/src/protocol.rs
+++ vm-migration/src/protocol.rs
@@ -211,7 +211,7 @@ impl Response {
 }
 
 #[repr(C)]
-#[derive(Clone, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Serialize, Deserialize, PartialEq, Eq)]
 pub struct MemoryRange {
     pub gpa: u64,
     pub length: u64,
@@ -244,12 +244,85 @@ impl MemoryRange {
     }
 }
 
-#[derive(Clone, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct MemoryRangeTable {
     data: Vec<MemoryRange>,
 }
 
+#[derive(Debug, Clone, Default)]
+struct MemoryRangeTableIterator {
+    chunk_size: u64,
+    data: Vec<MemoryRange>,
+}
+
+impl MemoryRangeTableIterator {
+    pub fn new(table: &MemoryRangeTable, chunk_size: u64) -> Self {
+        MemoryRangeTableIterator {
+            chunk_size,
+            data: table.data.clone(),
+        }
+    }
+}
+
+impl Iterator for MemoryRangeTableIterator {
+    type Item = MemoryRangeTable;
+
+    /// Return the next memory range in the table, making sure that
+    /// the returned range is not larger than `chunk_size`.
+    ///
+    /// **Note**: Do not rely on the order of the ranges returned by this
+    /// iterator. This allows for a more efficient implementation.
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut ranges: Vec<MemoryRange> = vec![];
+        let mut ranges_size: u64 = 0;
+
+        loop {
+            assert!(ranges_size <= self.chunk_size);
+
+            if ranges_size == self.chunk_size || self.data.is_empty() {
+                break;
+            }
+
+            if let Some(range) = self.data.pop() {
+                let next_range: MemoryRange = if ranges_size + range.length > self.chunk_size {
+                    // How many bytes we need to put back into the table.
+                    let leftover_bytes = ranges_size + range.length - self.chunk_size;
+                    assert!(leftover_bytes <= range.length);
+                    let returned_bytes = range.length - leftover_bytes;
+                    assert!(returned_bytes <= range.length);
+                    assert!(leftover_bytes + returned_bytes == range.length);
+
+                    self.data.push(MemoryRange {
+                        gpa: range.gpa + returned_bytes,
+                        length: leftover_bytes,
+                    });
+                    MemoryRange {
+                        gpa: range.gpa,
+                        length: returned_bytes,
+                    }
+                } else {
+                    range
+                };
+
+                ranges_size += next_range.length;
+                ranges.push(next_range);
+            }
+        }
+
+        if ranges.is_empty() {
+            None
+        } else {
+            Some(MemoryRangeTable { data: ranges })
+        }
+    }
+}
+
 impl MemoryRangeTable {
+    /// Partitions the table into chunks of at most `chunk_size` bytes.
+    pub fn partition(&self, chunk_size: u64) -> impl Iterator<Item = MemoryRangeTable> {
+        MemoryRangeTableIterator::new(self, chunk_size)
+    }
+
     pub fn from_bitmap(
         bitmap: impl IntoIterator<Item = u64>,
         start_addr: u64,
@@ -314,5 +387,64 @@ impl MemoryRangeTable {
             data.extend(table.data);
         }
         Self { data }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_memory_range_table() {
+        let mut table = MemoryRangeTable::default();
+        // Test blocks that are shorter than the chunk size.
+        table.push(MemoryRange {
+            gpa: 0,
+            length: 1 << 10,
+        });
+        // Test blocks that are longer than the chunk size.
+        table.push(MemoryRange {
+            gpa: 0x1000,
+            length: 3 << 20,
+        });
+        // And add another blocks, so we get a chunk that spans two memory
+        // ranges.
+        table.push(MemoryRange {
+            gpa: 4 << 20,
+            length: 1 << 20,
+        });
+
+        let table = table; // drop mut
+
+        let chunks = table
+            .partition(2 << 20)
+            .map(|table| table.data)
+            .collect::<Vec<_>>();
+
+        // The implementation currently returns the ranges in reverse order. If
+        // this tests becomes more complex, we can compare everything as sets.
+        assert_eq!(
+            chunks,
+            vec![
+                vec![
+                    MemoryRange {
+                        gpa: 4 << 20,
+                        length: 1 << 20
+                    },
+                    MemoryRange {
+                        gpa: 0x1000,
+                        length: 1 << 20
+                    }
+                ],
+                vec![MemoryRange {
+                    gpa: 0x1000 + (1 << 20),
+                    length: 2 << 20
+                },],
+                vec![MemoryRange {
+                    gpa: 0,
+                    length: 1 << 10
+                }]
+            ]
+        );
     }
 }
