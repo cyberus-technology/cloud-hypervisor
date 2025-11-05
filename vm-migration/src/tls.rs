e1bbd54f453fe29bb65f59ba0beb9eb60d49f569
Sebastian Eydam <sebastian.eydam@cyberus-technology.de>
2025-11-05 14:25:28 +0100

    vm-migration: add the server side of the TLS encryption
    This is the TLS server side, which will be the live migration target.
    
    On-behalf-of: SAP sebastian.eydam@sap.com
    Signed-off-by: Sebastian Eydam <sebastian.eydam@cyberus-technology.de>

--- vm-migration/src/tls.rs
+++ vm-migration/src/tls.rs
@@ -9,8 +9,10 @@ use std::path::Path;
 use std::sync::Arc;
 
 use rustls::pki_types::pem::PemObject;
-use rustls::pki_types::{CertificateDer, InvalidDnsNameError, ServerName};
-use rustls::{ClientConfig, ClientConnection, RootCertStore, StreamOwned};
+use rustls::pki_types::{CertificateDer, InvalidDnsNameError, PrivateKeyDer, ServerName};
+use rustls::{
+    ClientConfig, ClientConnection, RootCertStore, ServerConfig, ServerConnection, StreamOwned,
+};
 use thiserror::Error;
 use vm_memory::bitmap::BitmapSlice;
 use vm_memory::io::{ReadVolatile, WriteVolatile};
@@ -41,12 +43,14 @@ pub enum TlsError {
 #[derive(Debug)]
 pub enum TlsStream {
     Client(StreamOwned<ClientConnection, TcpStream>),
+    Server(StreamOwned<ServerConnection, TcpStream>),
 }
 
 impl Read for TlsStream {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         match self {
             TlsStream::Client(s) => s.read(buf),
+            TlsStream::Server(s) => s.read(buf),
         }
     }
 }
@@ -55,11 +59,13 @@ impl Write for TlsStream {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         match self {
             TlsStream::Client(s) => s.write(buf),
+            TlsStream::Server(s) => s.write(buf),
         }
     }
     fn flush(&mut self) -> io::Result<()> {
         match self {
             TlsStream::Client(s) => s.flush(),
+            TlsStream::Server(s) => s.flush(),
         }
     }
 }
@@ -71,6 +77,7 @@ impl AsFd for TlsStream {
     fn as_fd(&self) -> BorrowedFd<'_> {
         match self {
             TlsStream::Client(s) => s.get_ref().as_fd(),
+            TlsStream::Server(s) => s.get_ref().as_fd(),
         }
     }
 }
@@ -96,6 +103,50 @@ impl WriteVolatile for TlsStream {
         let n = vs.copy_to(&mut tmp[..]);
         let n = Write::write(self, &tmp[..n]).unwrap();
         Ok(n)
+    }
+}
+
+// A small wrapper to be put into ReceiveListener::Tls. It carries the
+// TLS-Config and creates a TlsStream after the TcpConnection accepted a
+// connection.
+#[derive(Debug, Clone)]
+pub struct TlsConnectionWrapper {
+    config: Arc<ServerConfig>,
+}
+
+impl TlsConnectionWrapper {
+    pub fn new(cert_dir: &Path) -> Self {
+        let certs = CertificateDer::pem_file_iter(cert_dir.join("server-cert.pem"))
+            .unwrap()
+            .map(|cert| cert.unwrap())
+            .collect();
+        let key = PrivateKeyDer::from_pem_file(cert_dir.join("server-key.pem")).unwrap();
+        let config = ServerConfig::builder()
+            .with_no_client_auth()
+            .with_single_cert(certs, key)
+            .map_err(TlsError::RustlsError)
+            .unwrap();
+        let config = Arc::new(config);
+        Self { config }
+    }
+
+    pub fn wrap(&self, socket: TcpStream) -> std::result::Result<TlsStream, MigratableError> {
+        let conn = ServerConnection::new(self.config.clone()).map_err(TlsError::RustlsError)?;
+
+        let mut tls = StreamOwned::new(conn, socket);
+        while tls.conn.is_handshaking() {
+            let (rd, wr) = tls
+                .conn
+                .complete_io(&mut tls.sock)
+                .map_err(TlsError::RustlsIoError)?;
+            if rd == 0 && wr == 0 {
+                Err(TlsError::HandshakeError(
+                    "EOF during TLS handshake".to_string(),
+                ))?;
+            }
+        }
+
+        Ok(TlsStream::Server(tls))
     }
 }
 
