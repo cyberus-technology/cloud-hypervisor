e165ee42fee84b97d0c21f6a70677897e0bcb36d
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-11-21 10:48:15 +0100

    vmm: migration: prepare EventFd for async migration events
    This is a pre-requisite for the following commit which puts the
    migration into a dedicated thread. It allows the VMM to react to
    migration events (success/failure).
    
    The commit series was inspired by @ljcore [0] but was changed quite
    significantly.
    
    [0] https://github.com/cloud-hypervisor/cloud-hypervisor/pull/7038
    
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>
    On-behalf-of: SAP philipp.schuster@sap.com

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -254,6 +254,7 @@ pub enum EpollDispatch {
     Api = 2,
     ActivateVirtioDevices = 3,
     Debug = 4,
+    CheckMigration = 5,
     Unknown,
 }
 
@@ -266,6 +267,7 @@ impl From<u64> for EpollDispatch {
             2 => Api,
             3 => ActivateVirtioDevices,
             4 => Debug,
+            5 => CheckMigration,
             _ => Unknown,
         }
     }
@@ -748,6 +750,7 @@ pub struct Vmm {
     original_termios_opt: Arc<Mutex<Option<termios>>>,
     console_resize_pipe: Option<Arc<File>>,
     console_info: Option<ConsoleInfo>,
+    check_migration_evt: EventFd,
 }
 
 /// Wait for a file descriptor to become readable. In this case, we return
@@ -1514,6 +1517,7 @@ impl Vmm {
         let mut epoll = EpollContext::new().map_err(Error::Epoll)?;
         let reset_evt = EventFd::new(EFD_NONBLOCK).map_err(Error::EventFdCreate)?;
         let activate_evt = EventFd::new(EFD_NONBLOCK).map_err(Error::EventFdCreate)?;
+        let check_migration_evt = EventFd::new(EFD_NONBLOCK).map_err(Error::EventFdCreate)?;
 
         epoll
             .add_event(&exit_evt, EpollDispatch::Exit)
@@ -1536,6 +1540,10 @@ impl Vmm {
             .add_event(&debug_evt, EpollDispatch::Debug)
             .map_err(Error::Epoll)?;
 
+        epoll
+            .add_event(&check_migration_evt, EpollDispatch::CheckMigration)
+            .map_err(Error::Epoll)?;
+
         Ok(Vmm {
             epoll,
             exit_evt,
@@ -1556,6 +1564,7 @@ impl Vmm {
             original_termios_opt: Arc::new(Mutex::new(None)),
             console_resize_pipe: None,
             console_info: None,
+            check_migration_evt,
         })
     }
 
@@ -2303,6 +2312,14 @@ impl Vmm {
         }
     }
 
+    /// Checks the migration result.
+    ///
+    /// This should be called when the migration thread indicated a state
+    /// change (and therefore, its termination). The function checks the result
+    /// of that thread and either shuts down the VMM on success or keeps the VM
+    /// and the VMM running on migration failure.
+    fn check_migration_result(&mut self) {}
+
     fn control_loop(
         &mut self,
         api_receiver: &Receiver<ApiRequest>,
@@ -2393,6 +2410,14 @@ impl Vmm {
                     }
                     #[cfg(not(feature = "guest_debug"))]
                     EpollDispatch::Debug => {}
+                    EpollDispatch::CheckMigration => {
+                        info!("VM migration check event");
+                        // Consume the event.
+                        self.check_migration_evt
+                            .read()
+                            .map_err(Error::EventFdRead)?;
+                        self.check_migration_result();
+                    }
                 }
             }
         }
