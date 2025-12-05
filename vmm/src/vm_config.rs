0eb8cea827b5abddbaa0279af1eb1f384bbe6de4
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2025-12-05 08:27:10 +0100

    vmm: Introduce `addr` argument to PCI device configs and update parser
    Updates all config structs in order to make the new config option
    available to all PCI device. Additionally, update the parser so the new
    option becomes available on the CLI.
    
    Signed-off-by: Pascal Scholz <pascal.scholz@cyberus-technology.de>
    On-behalf-of: SAP pascal.scholz@sap.com

--- vmm/src/vm_config.rs
+++ vmm/src/vm_config.rs
@@ -284,6 +284,8 @@ pub struct DiskConfig {
     pub serial: Option<String>,
     #[serde(default)]
     pub queue_affinity: Option<Vec<VirtQueueAffinity>>,
+    #[serde(default)]
+    pub bdf_device: Option<u8>,
 }
 
 impl ApplyLandlock for DiskConfig {
@@ -344,6 +346,8 @@ pub struct NetConfig {
     pub offload_ufo: bool,
     #[serde(default = "default_netconfig_true")]
     pub offload_csum: bool,
+    #[serde(default)]
+    pub bdf_device: Option<u8>,
 }
 
 pub fn default_netconfig_true() -> bool {
@@ -390,6 +394,8 @@ pub struct RngConfig {
     pub src: PathBuf,
     #[serde(default)]
     pub iommu: bool,
+    #[serde(default)]
+    pub bdf_device: Option<u8>,
 }
 
 pub const DEFAULT_RNG_SOURCE: &str = "/dev/urandom";
@@ -399,6 +405,7 @@ impl Default for RngConfig {
         RngConfig {
             src: PathBuf::from(DEFAULT_RNG_SOURCE),
             iommu: false,
+            bdf_device: None,
         }
     }
 }
@@ -420,6 +427,8 @@ pub struct BalloonConfig {
     /// Option to enable free page reporting from the guest.
     #[serde(default)]
     pub free_page_reporting: bool,
+    #[serde(default)]
+    pub bdf_device: Option<u8>,
 }
 
 #[cfg(feature = "pvmemcontrol")]
@@ -438,6 +447,8 @@ pub struct FsConfig {
     pub id: Option<String>,
     #[serde(default)]
     pub pci_segment: u16,
+    #[serde(default)]
+    pub bdf_device: Option<u8>,
 }
 
 pub fn default_fsconfig_num_queues() -> usize {
@@ -468,6 +479,8 @@ pub struct PmemConfig {
     pub id: Option<String>,
     #[serde(default)]
     pub pci_segment: u16,
+    #[serde(default)]
+    pub bdf_device: Option<u8>,
 }
 
 impl ApplyLandlock for PmemConfig {
@@ -498,6 +511,9 @@ pub struct ConsoleConfig {
     pub iommu: bool,
     pub socket: Option<PathBuf>,
     pub url: Option<String>,
+    /// PCI BDF to attach the console in the guest to
+    #[serde(default)]
+    pub bdf_device: Option<u8>,
 }
 
 pub fn default_consoleconfig_file() -> Option<PathBuf> {
@@ -603,6 +619,8 @@ pub struct VdpaConfig {
     pub id: Option<String>,
     #[serde(default)]
     pub pci_segment: u16,
+    #[serde(default)]
+    pub bdf_device: Option<u8>,
 }
 
 pub fn default_vdpaconfig_num_queues() -> usize {
@@ -626,6 +644,8 @@ pub struct VsockConfig {
     pub id: Option<String>,
     #[serde(default)]
     pub pci_segment: u16,
+    #[serde(default)]
+    pub bdf_device: Option<u8>,
 }
 
 impl ApplyLandlock for VsockConfig {
@@ -859,6 +879,7 @@ pub fn default_serial() -> ConsoleConfig {
         iommu: false,
         socket: None,
         url: None,
+        bdf_device: None,
     }
 }
 
@@ -869,6 +890,7 @@ pub fn default_console() -> ConsoleConfig {
         iommu: false,
         socket: None,
         url: None,
+        bdf_device: None,
     }
 }
 
