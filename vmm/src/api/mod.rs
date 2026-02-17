902e68a5a9744125ae9b51a2aa573ce444b39cdd
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2026-02-17 17:05:06 +0100

    vmm: Supply a default for `VmReceiveMigrationData` members
    If no defaults are supplied for `VmReceiveMigrationData`
    deserialization, deserialization can fail.
    
    Signed-off-by: Pascal Scholz <pascal.scholz@cyberus-technology.de>
    On-behalf-of: SAP pascal.scholz@sap.com

--- vmm/src/api/mod.rs
+++ vmm/src/api/mod.rs
@@ -266,11 +266,13 @@ pub struct VmReceiveMigrationData {
     /// migration. Example: "192.168.1.1:2222".
     pub tcp_serial_url: Option<String>,
     /// Map with new network FDs on the new host.
+    #[serde(default)]
     pub net_fds: Vec<RestoredNetConfig>,
     /// Directory containing the TLS server certificate (server-cert.pem) and TLS server key (server-key.pem).
     #[serde(default)]
     pub tls_dir: Option<PathBuf>,
     /// Optional memory zone reconfiguration data
+    #[serde(default)]
     pub zones: Vec<MemoryZoneConfig>,
 }
 
