e4039c93c6be64387b3933152edccd1bd9896e66
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-02-18 11:53:10 +0100

    misc: move MSR retrieval to Hypervisor trait (KVM)
    This refactors MSR handling for KVM by moving the retrieval logic into
    the Hypervisor trait. It streamlines the integration with CpuId entries
    and simplifies the implementation of CpuProfiles (follow-up work).
    
    The MSRs vector acts as a buffer: supported MSRs are written during VM
    snapshot and later restored by KVM (for example, after live migration).
    With CPU profiles, some of these MSRs may become invalid, so we need a
    mechanism to update the MSR buffer accordingly.
    
    Since the hypervisor crate (where KvmVm resides) has no awareness of CPU
    profiles, it is more appropriate for the buffer to live in CpuManager,
    allowing dynamic updates based on the active CPU profile.
    
    Currently, this change only addresses KVM for rapid progress in our
    fork. Upstreaming will require proper consideration for other
    hypervisors, e.g., MSHV.
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- hypervisor/src/vm.rs
+++ hypervisor/src/vm.rs
@@ -28,6 +28,7 @@ use crate::arch::aarch64::gic::{Vgic, VgicConfig};
 use crate::arch::riscv64::aia::{Vaia, VaiaConfig};
 #[cfg(feature = "tdx")]
 use crate::arch::x86::CpuIdEntry;
+use crate::arch::x86::MsrEntry;
 use crate::cpu::Vcpu;
 use crate::{IoEventAddress, IrqRoutingEntry};
 
@@ -322,7 +323,12 @@ pub trait Vm: Send + Sync + Any {
     /// Unregister an event that will, when signaled, trigger the `gsi` IRQ.
     fn unregister_irqfd(&self, fd: &EventFd, gsi: u32) -> Result<()>;
     /// Creates a new KVM vCPU file descriptor and maps the memory corresponding
-    fn create_vcpu(&self, id: u32, vm_ops: Option<Arc<dyn VmOps>>) -> Result<Box<dyn Vcpu>>;
+    fn create_vcpu(
+        &self,
+        id: u32,
+        vm_ops: Option<Arc<dyn VmOps>>,
+        #[cfg(target_arch = "x86_64")] msrs: Vec<MsrEntry>,
+    ) -> Result<Box<dyn Vcpu>>;
     #[cfg(target_arch = "aarch64")]
     fn create_vgic(&self, config: &VgicConfig) -> Result<Arc<Mutex<dyn Vgic>>>;
     #[cfg(target_arch = "riscv64")]
