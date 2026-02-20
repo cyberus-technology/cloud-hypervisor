a0d6833bd97c972b40f3b303f37b8abae66274a8
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2026-02-20 15:04:02 +0100

    hypervisor: msr_filter method on Vm trait
    In order to ensure that MSRs that are not compatible with a given
    CPU profile do no get accessed by the guests we need to introduce
    functionality to deny such MSRs via filters.
    
    The implementation introduced here is mostly a temporary workaround
    until https://github.com/rust-vmm/kvm/pull/359 is integrated in
    CHV.
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- hypervisor/src/mshv/mod.rs
+++ hypervisor/src/mshv/mod.rs
@@ -2620,4 +2620,13 @@ impl vm::Vm for MshvVm {
         }
         Ok(())
     }
+
+    #[cfg(target_arch = "x86_64")]
+    fn msr_filter<'a>(
+        &self,
+        _filter: &[crate::MsrFilterRange<'a>],
+        _default_deny: bool,
+    ) -> vm::Result<()> {
+        todo!()
+    }
 }
