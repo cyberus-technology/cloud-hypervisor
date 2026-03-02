6e0465d3525ac122aa692b26582d5793807ceb98
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-03-02 14:51:22 +0100

    vmm: migration: early cancellation (add more checks)
    This adds multiple points in the migration path where the migration can
    be canceled early. The pre-copy phase is not addressed here and will
    follow in the next commit!
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -1419,6 +1419,7 @@ impl Vmm {
         is_converged: impl Fn(&MemoryMigrationContext) -> result::Result<bool, MigratableError>,
         mem_send: &mut SendAdditionalConnections,
         postponed_lifecycle_event: &Mutex<Option<PostMigrationLifecycleEvent>>,
+        return_if_cancelled_cb: &impl Fn(&mut SocketStream) -> result::Result<(), MigratableError>,
     ) -> result::Result<MemoryRangeTable /* remaining */, MigratableError> {
         let update_migration_progress = |s: &mut MemoryMigrationContext, vm: &Vm| {
             let mut lock = MIGRATION_PROGRESS_SNAPSHOT.lock().unwrap();
@@ -1451,6 +1452,8 @@ impl Vmm {
         };
 
         loop {
+            return_if_cancelled_cb(socket)?;
+
             // todo: check if auto-converge is enabled at all?
             if Self::can_increase_autoconverge_step(ctx)
                 && vm.throttle_percent() < AUTO_CONVERGE_MAX
@@ -1486,7 +1489,7 @@ impl Vmm {
 
             // Send the current dirty pages
             let transfer_begin = Instant::now();
-            mem_send.send_memory(iteration_table, socket)?;
+            mem_send.send_memory(iteration_table, socket, return_if_cancelled_cb)?;
             let transfer_duration = transfer_begin.elapsed();
             ctx.update_metrics_after_transfer(transfer_begin, transfer_duration);
 
@@ -1624,6 +1627,7 @@ impl Vmm {
         mem_send: &mut SendAdditionalConnections,
         ctx: &mut OngoingMigrationContext,
         postponed_lifecycle_event: &Mutex<Option<PostMigrationLifecycleEvent>>,
+        return_if_cancelled_cb: &impl Fn(&mut SocketStream) -> result::Result<(), MigratableError>,
     ) -> result::Result<(), MigratableError> {
         let mut mem_ctx = MemoryMigrationContext::new();
 
@@ -1636,6 +1640,7 @@ impl Vmm {
             |ctx| Self::is_precopy_converged(ctx, send_data_migration),
             mem_send,
             postponed_lifecycle_event,
+            return_if_cancelled_cb,
         )?;
         let downtime_begin = Instant::now();
         // End throttle thread
@@ -1658,7 +1663,7 @@ impl Vmm {
 
             mem_ctx.update_metrics_before_transfer(iteration_begin, &final_table);
             let transfer_begin = Instant::now();
-            mem_send.send_memory(final_table, socket)?;
+            mem_send.send_memory(final_table, socket, return_if_cancelled_cb)?;
             let transfer_duration = transfer_begin.elapsed();
             mem_ctx.update_metrics_after_transfer(transfer_begin, transfer_duration);
             mem_ctx.iteration += 1;
@@ -1716,6 +1721,8 @@ impl Vmm {
             MigratableError::MigrateSend(anyhow!("Error starting migration (got bad response)")),
         )?;
 
+        return_if_cancelled_cb(&mut socket)?;
+
         // Send config
         let vm_config = vm.get_config();
         #[cfg(all(feature = "kvm", target_arch = "x86_64"))]
@@ -1754,6 +1761,8 @@ impl Vmm {
             .map_err(MigratableError::MigrateSend)?
         };
 
+        return_if_cancelled_cb(&mut socket)?;
+
         if send_data_migration.local {
             match &mut socket {
                 SocketStream::Unix(unix_socket) => {
@@ -1773,6 +1782,8 @@ impl Vmm {
             }
         }
 
+        return_if_cancelled_cb(&mut socket)?;
+
         let vm_migration_config = VmMigrationConfig {
             vm_config,
             #[cfg(all(feature = "kvm", target_arch = "x86_64"))]
@@ -1780,6 +1791,8 @@ impl Vmm {
             memory_manager_data: vm.memory_manager_data(),
         };
         transport::send_config(&mut socket, &vm_migration_config)?;
+
+        return_if_cancelled_cb(&mut socket)?;
 
         // Let every Migratable object know about the migration being started.
         vm.start_migration()?;
@@ -1811,6 +1824,7 @@ impl Vmm {
                 &mut mem_send,
                 &mut ctx,
                 postponed_lifecycle_event,
+                &return_if_cancelled_cb,
             )
             .inspect_err(|_| {
                 // Calling cleanup multiple times is fine, thus here we just make sure
