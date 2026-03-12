e72e93d3e24ae9c184f6f30f35259bb1e94369f3
Peter Oskolkov <posk@google.com>
2026-03-12 09:50:37 -0700

    virtio-devices: introduce ActivationContext for device activation
    Signed-off-by: Peter Oskolkov <posk@google.com>
    (cherry picked from commit f77c6ef78bdca486eec0e6d81033af7082f9997d)

--- virtio-devices/src/device.rs
+++ virtio-devices/src/device.rs
@@ -54,6 +54,12 @@ pub struct VirtioSharedMemoryList {
     pub region_list: Vec<VirtioSharedMemory>,
 }
 
+pub struct ActivationContext {
+    pub mem: GuestMemoryAtomic<GuestMemoryMmap>,
+    pub interrupt_cb: Arc<dyn VirtioInterrupt>,
+    pub queues: Vec<(usize, Queue, EventFd)>,
+}
+
 /// Trait for virtio devices to be driven by a virtio transport.
 ///
 /// The lifecycle of a virtio device is to be moved to a virtio transport, which will then query the
@@ -95,12 +101,7 @@ pub trait VirtioDevice: Send {
     }
 
     /// Activates this device for real usage.
-    fn activate(
-        &mut self,
-        mem: GuestMemoryAtomic<GuestMemoryMmap>,
-        interrupt_evt: Arc<dyn VirtioInterrupt>,
-        queues: Vec<(usize, Queue, EventFd)>,
-    ) -> ActivateResult;
+    fn activate(&mut self, context: ActivationContext) -> ActivateResult;
 
     /// Optionally deactivates this device and returns ownership of the guest memory map, interrupt
     /// event, and queue events.
