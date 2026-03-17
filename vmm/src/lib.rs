8c23a50e865a3f815b049b5f4c08d320cedb6d1d
Sebastian Eydam <sebastian.eydam@cyberus-technology.de>
2026-03-17 11:28:38 +0100

    vmm: always use KeepAliveStream for main connection
    On-behalf-of: SAP sebastian.eydam@sap.com
    Signed-off-by: Sebastian Eydam <sebastian.eydam@cyberus-technology.de>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -1776,8 +1776,7 @@ fn send_migration_socket(
             }
         };
 
-        // If we use multiple TCP connections, we have to send periodic keep alive messages. Thus, we create a KeepAliveStream.
-        if send_data_migration.connections.get() > 1 && main_connection {
+        if main_connection {
             return Ok(SocketStream::KeepAlive(
                 KeepAliveStream::new(socket, MIGRATION_WRITE_TIMEOUT_DURATION, true)
                     .context("Error creating keep alive sender")
