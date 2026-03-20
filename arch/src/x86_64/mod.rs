60293b46b64fb00aa254aa2c9db350dac7c95e20
Oliver Anderson <oliver.anderson@cyberus-technology.de>
2026-03-20 11:57:01 +0100

    arch: Helper functions for u32 hex (de-) serialization
    These helper functions will later be used to (de-) serialize CPUID
    leaves and MSR register addresses.
    
    Signed-off-by: Oliver Anderson <oliver.anderson@cyberus-technology.de>
    On-behalf-of: SAP oliver.anderson@sap.com

--- arch/src/x86_64/mod.rs
+++ arch/src/x86_64/mod.rs
@@ -18,6 +18,7 @@ pub mod regs;
 #[cfg(feature = "tdx")]
 pub mod tdx;
 
+mod helpers;
 mod mpspec;
 mod mptable;
 mod smbios;
@@ -25,6 +26,7 @@ mod smbios;
 use std::arch::x86_64;
 use std::mem;
 
+use helpers::{deserialize_u32_hex, serialize_u32_hex};
 use hypervisor::arch::x86::{CPUID_FLAG_VALID_INDEX, CpuIdEntry};
 use hypervisor::{CpuVendor, HypervisorCpuError, HypervisorError};
 use linux_loader::loader::bootparam::{boot_params, setup_header};
