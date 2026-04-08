7bc726ad147fb2cc7b0d463c7fb9c326d5d153d8
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-04-08 16:25:51 +0200

    vmm: migration: longer accept timeout for receiver
    This massively simplifies local development as
    `ch-remote receive-migration` won't terminate after 5s. This does not
    have implications for libvirt/ch, as the ch driver will kill the process
    if the migration won't start.
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -328,6 +328,15 @@ const MIGRATION_READ_TIMEOUT_DURATION: Duration = {
     migration_read_timeout_duration
 };
 
+/// The timeout of the migration-receiver.
+///
+/// We set this to a relatively high number to ease local development with
+/// `ch-remote`. For production, this has no negative impacts as the management
+/// software has full control over the Cloud Hypervisor process and will kill
+/// the process on terminated migration. The timeout is used as a fallback
+/// if the management software doesn't kill the process correctly.
+const MIGRATION_ACCEPT_TIMEOUT_DURATION: Duration = Duration::from_secs(60);
+
 enum SocketStream {
     Unix(UnixStream),
     Tcp(TcpStream),
@@ -1134,8 +1143,12 @@ impl ReceiveListener {
         match self {
             ReceiveListener::Tcp(listener) => {
                 let socket = {
+                    info!(
+                        "Waiting for incoming migration (timeout {}s) ...",
+                        MIGRATION_ACCEPT_TIMEOUT_DURATION.as_secs()
+                    );
                     let socket =
-                        Self::accept_with_timeout(listener, MIGRATION_READ_TIMEOUT_DURATION)?;
+                        Self::accept_with_timeout(listener, MIGRATION_ACCEPT_TIMEOUT_DURATION)?;
 
                     socket.set_read_timeout(Some(MIGRATION_READ_TIMEOUT_DURATION))?;
                     socket.set_write_timeout(Some(MIGRATION_WRITE_TIMEOUT_DURATION))?;
@@ -1169,8 +1182,12 @@ impl ReceiveListener {
             }
             ReceiveListener::Tls(listener, conn) => {
                 let socket = {
+                    info!(
+                        "Waiting for incoming migration (timeout {}s) ...",
+                        MIGRATION_ACCEPT_TIMEOUT_DURATION.as_secs()
+                    );
                     let socket =
-                        Self::accept_with_timeout(listener, MIGRATION_READ_TIMEOUT_DURATION)?;
+                        Self::accept_with_timeout(listener, MIGRATION_ACCEPT_TIMEOUT_DURATION)?;
                     socket.set_read_timeout(Some(MIGRATION_READ_TIMEOUT_DURATION))?;
                     socket.set_write_timeout(Some(MIGRATION_WRITE_TIMEOUT_DURATION))?;
                     conn.wrap(socket)
