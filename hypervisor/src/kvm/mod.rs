59d99f292748687a7892ae6a2f16d37e4e5ef6a6
Rob Bradford <rbradford@meta.com>
2026-05-27 12:18:00 +0100

    hypervisor: Fix clippy: for_kv_map
    Fix use of a paired map iterator when only the values are cared about.
    
    Signed-off-by: Rob Bradford <rbradford@meta.com>
    (cherry picked from commit 673d4a441c3ee324113f609609c3874a2864ca77)

--- hypervisor/src/kvm/mod.rs
+++ hypervisor/src/kvm/mod.rs
@@ -1318,7 +1318,7 @@ impl vm::Vm for KvmVm {
     ///
     fn start_dirty_log(&self) -> vm::Result<()> {
         let dirty_log_slots = self.dirty_log_slots.read().unwrap();
-        for (_, s) in dirty_log_slots.iter() {
+        for s in dirty_log_slots.values() {
             let region = kvm_userspace_memory_region2 {
                 slot: s.slot,
                 guest_phys_addr: s.guest_phys_addr,
@@ -1344,7 +1344,7 @@ impl vm::Vm for KvmVm {
     ///
     fn stop_dirty_log(&self) -> vm::Result<()> {
         let dirty_log_slots = self.dirty_log_slots.read().unwrap();
-        for (_, s) in dirty_log_slots.iter() {
+        for s in dirty_log_slots.values() {
             let region = kvm_userspace_memory_region2 {
                 slot: s.slot,
                 guest_phys_addr: s.guest_phys_addr,
