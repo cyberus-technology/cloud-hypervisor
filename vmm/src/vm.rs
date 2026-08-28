73bd5f371adae69e1b4bb06c2be7cd61675779cc
Pascal Scholz <pascal.scholz@cyberus-technology.de>
2026-08-28 14:38:20 +0200

    devices: Move `populate_fw_cfg` arguments to dedicated helper struct
    To keep all data needed to initialize `fw_cfg` together, we move them
    from function arguments to a dedicated helper. As their number is going
    to grow, this will also help keeping the number of function arguments
    low.
    
    On-behalf-of: SAP pascal.scholz@sap.com
    Signed-off-by: Pascal Scholz <pascal.scholz@cyberus-technology.de>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -12,6 +12,8 @@
 //
 
 use std::collections::{BTreeMap, BTreeSet, HashMap};
+#[cfg(feature = "fw_cfg")]
+use std::ffi::CString;
 use std::fs::{File, OpenOptions};
 use std::io::{self, Seek, SeekFrom, Write};
 #[cfg(all(target_arch = "x86_64", feature = "guest_debug"))]
@@ -38,7 +40,7 @@ use devices::AcpiNotificationFlags;
 #[cfg(target_arch = "aarch64")]
 use devices::interrupt_controller;
 #[cfg(feature = "fw_cfg")]
-use devices::legacy::fw_cfg::FwCfgItem;
+use devices::legacy::fw_cfg::{FwCfgInitParams, FwCfgItem};
 use event_monitor::event;
 #[cfg(all(target_arch = "aarch64", feature = "guest_debug"))]
 use gdbstub_arch::aarch64::reg::AArch64CoreRegs as CoreRegs;
@@ -1222,7 +1224,7 @@ impl Vm {
                 .map_err(Error::MissingFwCfgKernelFile)?;
             kernel_option = kernel;
         }
-        let mut cmdline_option: Option<std::ffi::CString> = None;
+        let mut cmdline_option: Option<CString> = None;
         if fw_cfg_config.cmdline {
             let cmdline = Vm::generate_cmdline(
                 config.payload.as_ref().unwrap(),
@@ -1275,16 +1277,19 @@ impl Vm {
         let Some(fw_cfg) = device_manager_binding.fw_cfg() else {
             return Err(Error::FwCfgDisabled);
         };
+        let init = FwCfgInitParams {
+            e820_size: e820_option,
+            cmdline: cmdline_option,
+            kernel: kernel_option,
+            initramfs: initramfs_option,
+            item_list: fw_cfg_item_list_option,
+        };
 
         fw_cfg
             .lock()
             .unwrap()
             .populate_fw_cfg(
-                e820_option,
-                kernel_option,
-                initramfs_option,
-                cmdline_option,
-                fw_cfg_item_list_option,
+                init,
                 #[cfg(target_arch = "x86_64")]
                 kvm_sev_snp_enabled,
             )
