e451c9380aa9250fa3fa0bb0a2b2ab741a1bfe20
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-09-18 08:15:44 +0200

    vmm: vm: populate the TDVF sections completely
    Please note that there is no retry behavior in the underlying impl,
    but at least we notice if it fails [0].
    
    [0] https://github.com/rust-vmm/rust-vmm/issues/43
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -2761,7 +2761,9 @@ impl Vm {
                     firmware_file
                         .seek(SeekFrom::Start(section.data_offset as u64))
                         .map_err(Error::LoadTdvf)?;
-                    mem.read_volatile_from(
+                    // TODO although the name indicates, there is no retry internally: https://github.com/rust-vmm/rust-vmm/issues/43
+                    // For now, we prefer to fail loudly at least.
+                    mem.read_exact_volatile_from(
                         GuestAddress(section.address),
                         &mut firmware_file,
                         section.data_size as usize,
@@ -2784,7 +2786,7 @@ impl Vm {
 
                         let mut payload_header = linux_loader::bootparam::setup_header::default();
                         payload_file
-                            .read_volatile(&mut payload_header.as_bytes())
+                            .read_exact_volatile(&mut payload_header.as_bytes())
                             .unwrap();
 
                         if payload_header.header != 0x5372_6448 {
@@ -2798,7 +2800,9 @@ impl Vm {
                         }
 
                         payload_file.rewind().map_err(Error::LoadPayload)?;
-                        mem.read_volatile_from(
+                        // TODO although the name indicates, there is no retry internally: https://github.com/rust-vmm/rust-vmm/issues/43
+                        // For now, we prefer to fail loudly at least.
+                        mem.read_exact_volatile_from(
                             GuestAddress(section.address),
                             payload_file,
                             payload_size as usize,
