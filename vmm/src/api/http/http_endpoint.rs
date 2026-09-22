3cc95e1fdc2244cfacc2358bbe78a585112a5b10
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-09-22 18:14:02 +0200

    vmm: Revert "http api: add VmCancelMigration action"
    This reverts commit 9fa49fd84607f3edadc7e062376401ee974eb545.
    
    The import lists also carry VmPostMigrationAnnounce, which stays.
    
    This commit builds on the fork's own migration worker, which this
    series replaces with the upstream implementation. It is re-applied
    on top of the upstream worker later in this series.
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- vmm/src/api/http/http_endpoint.rs
+++ vmm/src/api/http/http_endpoint.rs
@@ -48,10 +48,9 @@ use crate::api::http::{EndpointHandler, HttpError, error_response};
 use crate::api::{
     AddDisk, ApiAction, ApiError, ApiRequest, NetConfig, VmAddDevice, VmAddFs,
     VmAddGenericVhostUser, VmAddNet, VmAddPmem, VmAddUserDevice, VmAddVdpa, VmAddVsock, VmBoot,
-    VmCancelMigration, VmConfig, VmCounters, VmDelete, VmMigrationProgress, VmNmi, VmPause,
-    VmPostMigrationAnnounce, VmPowerButton, VmReboot, VmReceiveMigration, VmReceiveMigrationData,
-    VmRemoveDevice, VmResize, VmResizeDisk, VmResizeZone, VmRestore, VmResume, VmSendMigration,
-    VmShutdown, VmSnapshot,
+    VmConfig, VmCounters, VmDelete, VmMigrationProgress, VmNmi, VmPause, VmPostMigrationAnnounce,
+    VmPowerButton, VmReboot, VmReceiveMigration, VmReceiveMigrationData, VmRemoveDevice, VmResize,
+    VmResizeDisk, VmResizeZone, VmRestore, VmResume, VmSendMigration, VmShutdown, VmSnapshot,
 };
 use crate::config::RestoreConfig;
 use crate::cpu::Error as CpuError;
@@ -419,7 +418,6 @@ vm_action_put_handler!(VmResume);
 vm_action_put_handler!(VmPostMigrationAnnounce);
 vm_action_put_handler!(VmPowerButton);
 vm_action_put_handler!(VmNmi);
-vm_action_put_handler!(VmCancelMigration);
 
 vm_action_put_handler_body!(VmAddDevice);
 vm_action_put_handler_body!(AddDisk);
