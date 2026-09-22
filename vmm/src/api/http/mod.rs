6e93ca89756ccf62b7f16af23b98cd278eaba99a
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-09-22 18:16:01 +0200

    vmm: Revert "add migration-progress HTTP endpoint"
    This reverts commit 2dd9618cfaa88f12cfd7cf6a6f52e070c18f4ea9.
    
    This commit builds on the fork's own migration worker, which this
    series replaces with the upstream implementation. It is re-applied
    on top of the upstream worker later in this series.
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- vmm/src/api/http/mod.rs
+++ vmm/src/api/http/mod.rs
@@ -29,10 +29,10 @@ use self::http_endpoint::{VmActionHandler, VmCreate, VmInfo, VmmPing, VmmShutdow
 use crate::api::VmCoredump;
 use crate::api::{
     AddDisk, ApiError, ApiRequest, VmAddDevice, VmAddFs, VmAddGenericVhostUser, VmAddNet,
-    VmAddPmem, VmAddUserDevice, VmAddVdpa, VmAddVsock, VmBoot, VmCounters, VmDelete,
-    VmMigrationProgress, VmNmi, VmPause, VmPostMigrationAnnounce, VmPowerButton, VmReboot,
-    VmReceiveMigration, VmRemoveDevice, VmResize, VmResizeDisk, VmResizeZone, VmRestore, VmResume,
-    VmSendMigration, VmShutdown, VmSnapshot,
+    VmAddPmem, VmAddUserDevice, VmAddVdpa, VmAddVsock, VmBoot, VmCounters, VmDelete, VmNmi,
+    VmPause, VmPostMigrationAnnounce, VmPowerButton, VmReboot, VmReceiveMigration, VmRemoveDevice,
+    VmResize, VmResizeDisk, VmResizeZone, VmRestore, VmResume, VmSendMigration, VmShutdown,
+    VmSnapshot,
 };
 use crate::landlock::Landlock;
 use crate::seccomp_filters::{Thread, get_seccomp_filter};
@@ -285,10 +285,6 @@ pub static HTTP_ROUTES: LazyLock<HttpRoutes> = LazyLock::new(|| {
     r.routes.insert(
         endpoint!("/vm.shutdown"),
         Box::new(VmActionHandler::new(&VmShutdown)),
-    );
-    r.routes.insert(
-        endpoint!("/vm.migration-progress"),
-        Box::new(VmMigrationProgress {}),
     );
     r.routes.insert(
         endpoint!("/vm.snapshot"),
